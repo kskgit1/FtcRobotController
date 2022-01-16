32c9dbc6a08504d0fc8c606f7cbf323a4e0983cc
kskgit1 <55639416+kskgit1@users.noreply.github.com>
2022-01-16 14:53:03 -0800

    Update TeleOp_v1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_v1.java
@@ -1,1 +1,120 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="TeleOp_v1", group="Linear Opmode")
+public class TeleOp_v1 extends LinearOpMode
+{
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //name motor variables
+    private DcMotor fldrive, frdrive, brdrive, bldrive, slides, arm;
+
+    @Override
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //map the motor variables to actual motors
+        fldrive = hardwareMap.get(DcMotor.class, "fldrive");
+        frdrive = hardwareMap.get(DcMotor.class, "frdrive");
+        brdrive = hardwareMap.get(DcMotor.class, "brdrive");
+        bldrive = hardwareMap.get(DcMotor.class, "bldrive");
+
+        slides = hardwareMap.get(DcMotor.class, "slides");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+        //set direction of motors
+        fldrive.setDirection(DcMotor.Direction.REVERSE);
+        frdrive.setDirection(DcMotor.Direction.FORWARD);
+        brdrive.setDirection(DcMotor.Direction.FORWARD);
+        bldrive.setDirection(DcMotor.Direction.REVERSE);
+
+        slides.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.FORWARD);
+
+        // slow mode
+        boolean slowmode = false;
+        boolean xControl = false;
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive())
+        {
+            // motion controls
+            double speed = gamepad1.left_stick_y;
+            double turn = -gamepad1.right_stick_x;
+            double strafe = -gamepad1.left_stick_x;
+
+            // slides controls
+            double slidesup = gamepad1.right_trigger;
+            double slidesdown = gamepad1.left_trigger;
+
+            // arm controls
+            double shootfor = gamepad1.left_trigger;
+            double shootback = gamepad2.right_trigger;
+
+            // intake & conveyer controls
+            double takein = gamepad2.right_stick_y;
+            double convey = gamepad2.left_stick_y;
+
+            //determine power for each motor
+            double fl = speed + turn + strafe;
+            double fr = speed - turn - strafe;
+            double br = speed - turn + strafe;
+            double bl = speed + turn - strafe;
+
+            double s = shootfor - shootback;
+            double a = armfor - armback;
+
+            //set power to drivetrain motors with range of -1 to 1
+            fldrive.setPower(Range.clip(fl, -1.0, 1.0));
+            frdrive.setPower(Range.clip(fr, -1.0, 1.0));
+            brdrive.setPower(Range.clip(br, -1.0, 1.0));
+            bldrive.setPower(Range.clip(bl, -1.0, 1.0));
+
+            //set power and direction for other motors
+            shooter.setPower(Range.clip(s, -1.0, 1.0));
+
+            if (gamepad1.b)
+            {
+                arm.setPower(Range.clip(a, -0.1, 0.1));
+            }
+            else
+            {
+                arm.setPower(Range.clip(a, -0.5, 0.5));
+            }
+
+            if (gamepad2.b)
+            {
+                conveyor.setPower(Range.clip(convey, -0.1, 0.1));
+                intake.setPower(Range.clip(takein, -0.1, 0.1));
+            }
+            else
+            {
+                conveyor.setPower(Range.clip(convey, -1.0, 1.0));
+                intake.setPower(Range.clip(takein, -1.0, 1.0));
+            }
+
+
+            //debug messages for each motor
+            telemetry.addData("fldrive",Double.toString(fldrive.getPower()));
+            telemetry.addData("frdrive",Double.toString(frdrive.getPower()));
+            telemetry.addData("brdrive",Double.toString(brdrive.getPower()));
+            telemetry.addData("bldrive",Double.toString(bldrive.getPower()));
+
+            telemetry.addData("slides",Double.toString(slides.getPower()));
+            telemetry.addData("arm",Double.toString(arm.getPower()));
+
+            telemetry.update();
+        }
+    }
+}
